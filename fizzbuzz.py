6d62de1833ad1f847d616ad03a6a2eca4c2d0541
Tarren Meyers <thetarrenmeyers@gmail.com>
2021-03-07 22:23:34 -0800

    Implementation of code for fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+def fizzbuzz():
+    result = ""
+    for i in range(1,101):
+        if ((i%3) == 0) and ((i%5) == 0):
+            result+="FizzBuzz"
+        elif (i%3) == 0:
+            result+="Fizz"
+
+        elif (i%5) == 0:
+            result+="Buzz"
+        else:
+            result+=str(i)
+        result+="\n"
+    return result
+
+#print(fizzbuzz())
